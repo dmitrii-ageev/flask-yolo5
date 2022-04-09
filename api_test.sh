3bdb3c2098a28b5c2a59408040bcae33b59594ae
Dmitrii Ageev <dmitrii@opsworks.ru>
2022-04-09 08:25:25 +0000

    Add a proper logger

--- api_test.sh
+++ api_test.sh
@@ -11,16 +11,16 @@ curl \
 
 echo
 echo " >>> Passing a simple image:"
-image=$(base64 -w40960 test_images/image.jpg)
+image=$(base64 -w0 test_images/image.jpg)
 curl \
   --header "Content-Type: application/json" \
   --request POST \
-  --data '{"name":"test_image.jpg","body":"'$image'"}' \
+  --data '{"normalisation":"on", "name":"test_image.jpg","body":"'$image'"}' \
   http://localhost:5000/api/inspect_image
 
 echo
 echo " >>> Passing a complex test image:"
-image=$(base64 -w40960 test_images/557C0F8B32AF5F8B50D4F560FF443B2A.jpg)
+image=$(base64 -w0 test_images/557C0F8B32AF5F8B50D4F560FF443B2A.jpg)
 curl \
   --header "Content-Type: application/json" \
   --request POST \
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# importing the sys module and adding path to yolo5 modules
 import os
 from flask import Flask, render_template, request, redirect, \
     url_for, abort, send_from_directory, jsonify
@@ -10,7 +9,9 @@ from PIL import Image
 import torch
 import cv2
 import numpy as np
-import base64
+from base64 import standard_b64decode
+from sys import getsizeof
+import logging
 
 # Constants
 YOLO_GIT_REPO = 'ultralytics/yolov5'
@@ -20,18 +21,21 @@ TEMPLATE_FILE = 'index.html'
 # Like this: MODEL_PATH = './models/model.pt'
 MODEL_PATH = ''
 MAX_PAYLOAD = 4096*4096
+LOG_LEVEL=logging.DEBUG
 
 # Flask application settings
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = MAX_PAYLOAD
 app.config['IMAGE_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = UPLOADS_PATH
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.jinja_env.auto_reload = True
 
 
 def init_model(model):
     # load the model
     if MODEL_PATH == '':
-        return torch.hub.load(YOLO_GIT_REPO, 'yolov5s')
+        return torch.hub.load(YOLO_GIT_REPO, 'yolov5x')
     else:
         return torch.hub.load(YOLO_GIT_REPO, 'custom',
                               path=MODEL_PATH, force_reload=True)
@@ -105,17 +109,20 @@ def upload(filename):
 @app.route('/api/inspect_image', methods=['POST'])
 def inspect_image():
     if request.is_json:
-        content = request.json
+        content = dict(request.json)
         if 'body' in content.keys() and 'name' in content.keys():
-            if len(content['body']) < 512 or \
-               len(content['body']) > MAX_PAYLOAD:
+            image_body_length = getsizeof(content['body'])
+            if image_body_length < 512 or \
+               image_body_length > MAX_PAYLOAD:
                 # Fail with 'Unprocessable entity'
+                logger.error(f"Length of the image body doesn't comply with requirements: {image_body_length}.")
                 abort(422)
 
             # Trying to decode the image body
             try:
-                image_body = base64.standard_b64decode(content['body'])
+                image_body = standard_b64decode(content['body'])
             except Exception:
+                logger.error(f'Cannot decode image body using base64 method: {Exception}.')
                 abort(422)
 
             image_name = content['name']
@@ -134,7 +141,23 @@ def inspect_image():
 
 
 if __name__ == '__main__':
+    # create logger
+    logger = logging.getLogger('flask-yolo5')
+    logger.setLevel(LOG_LEVEL)
+
+    # create console handler and set level to debug
+    console = logging.StreamHandler()
+    console.setLevel(LOG_LEVEL)
+    
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    # add formatter to console
+    console.setFormatter(formatter)
+
+    # add console to logger
+    logger.addHandler(console)
+
+    # init a model and start the application
     MODEL = init_model(MODEL_PATH)
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host='0.0.0.0')
